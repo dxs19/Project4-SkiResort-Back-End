a690e97a3cb8dbe4372e3fba6d0a177ea6f864ac
Daniel Shull <danielshull121@gmail.com>
2022-12-08 17:05:38 -0500

    More routes created

---
+++ controllers/UserController.js
@@ -0,0 +1,26 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll()
+        res.send(users)
+    } catch (error) {
+        throw error
+    }
+}
+
+const getUserById = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.user_id, {
+        })
+        res.send(user)
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserById
+}
